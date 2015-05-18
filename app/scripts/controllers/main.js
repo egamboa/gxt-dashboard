38479d59c080fdc0db4dad3f9cbef5085d33f776
etb811ac <etb811ac@aol.com>
2015-05-18 10:50:25 -0600

    checkout 3 desktop

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gxtDashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gxtDashboardApp
+ */
+angular.module('gxtDashboardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
