262743b280abeb5d8a6c228be7628dea5f1ef7b0
etb811ac <etb811ac@aol.com>
2015-06-16 10:35:33 -0600

    releases update
    not final version

--- src/assets/javascript/javascript.js
+++ src/assets/javascript/javascript.js
@@ -1,3 +1,9 @@
+//general vars
+
+var plus = true;
+
+
+
 //slider initializer
 $("#releases-slider").slick({
 	infinite: false,
@@ -14,3 +20,41 @@ $("#releases-slider").addClass("customStyle");
 
 //mobile touch enable
 $('body').bind('touchstart', function() {});
+
+//projects show on release module function
+$('.release-projects-btn').click( function(){
+	// change icon button
+	if(plus){
+		$('.release-projects-btn').addClass('.btn-minus').removeClass('.btn-plus');
+		plus = false;
+		console.log(plus);
+	}else{
+		$('.release-projects-btn').addClass('.btn-plus').removeClass('.btn-minus');
+		plus = true
+		console.log(plus);
+	}
+
+	//opens, close projects
+	if ($('.release-project').css('opacity') != 1) {
+		$('.release-project').css({
+			display: 'table'
+		});
+		$('.release-project').animate({
+			opacity: 1
+		},500, function () {
+			
+		});
+
+	} else{
+		$('.release-project').animate({
+			opacity: 0
+		},500, function () {
+			$('.release-project').css({
+				display: 'none'
+			});
+		});
+
+	};
+
+	
+})
